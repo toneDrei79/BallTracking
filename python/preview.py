729f8520a32d86809e689947b7c5ebae03c8bc4f
toneDrei79 <tone0709@icloud.com>
2023-06-07 11:10:55 +0200

    remove depthai from  preview aswell

--- python/preview.py
+++ python/preview.py
@@ -4,43 +4,55 @@ import depthai as dai
 
 
 def main():
-    # Create pipeline
-    pipeline = dai.Pipeline()
+    cap = cv2.VideoCapture(0)
 
-    # Define source and output
-    camRgb = pipeline.create(dai.node.ColorCamera)
-    xoutRgb = pipeline.create(dai.node.XLinkOut)
-    xoutRgb.setStreamName("rgb")
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        cv2.imshow('rgb', frame)
 
-    # Properties
-    camRgb.setPreviewSize(1920, 1080)
-    camRgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
-    camRgb.setInterleaved(False)
-    camRgb.setColorOrder(dai.ColorCameraProperties.ColorOrder.BGR)
-    camRgb.setFps(30)
+        if cv2.waitKey(1) == ord('q'):
+            break
 
-    # Linking
-    camRgb.preview.link(xoutRgb.input)
 
-    # Connect to device and start pipeline
-    with dai.Device(pipeline) as device:
-        print('Connected cameras:', device.getConnectedCameraFeatures())
-        print('Usb speed:', device.getUsbSpeed().name)
-        if device.getBootloaderVersion() is not None:
-            print('Bootloader version:', device.getBootloaderVersion())
-        print('Device name:', device.getDeviceName())
+    # # Create pipeline
+    # pipeline = dai.Pipeline()
 
-        # Output queue will be used to get the rgb frames from the output defined above
-        qRgb = device.getOutputQueue(name="rgb", maxSize=4, blocking=False)
+    # # Define source and output
+    # camRgb = pipeline.create(dai.node.ColorCamera)
+    # xoutRgb = pipeline.create(dai.node.XLinkOut)
+    # xoutRgb.setStreamName("rgb")
 
-        while True:
-            inRgb = qRgb.get()  # blocking call, will wait until a new data has arrived
+    # # Properties
+    # camRgb.setPreviewSize(1920, 1080)
+    # camRgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
+    # camRgb.setInterleaved(False)
+    # camRgb.setColorOrder(dai.ColorCameraProperties.ColorOrder.BGR)
+    # camRgb.setFps(30)
 
-            # Retrieve 'bgr' (opencv format) frame
-            cv2.imshow("rgb", inRgb.getCvFrame())
+    # # Linking
+    # camRgb.preview.link(xoutRgb.input)
 
-            if cv2.waitKey(1) == ord('q'):
-                break
+    # # Connect to device and start pipeline
+    # with dai.Device(pipeline) as device:
+    #     print('Connected cameras:', device.getConnectedCameraFeatures())
+    #     print('Usb speed:', device.getUsbSpeed().name)
+    #     if device.getBootloaderVersion() is not None:
+    #         print('Bootloader version:', device.getBootloaderVersion())
+    #     print('Device name:', device.getDeviceName())
+
+    #     # Output queue will be used to get the rgb frames from the output defined above
+    #     qRgb = device.getOutputQueue(name="rgb", maxSize=4, blocking=False)
+
+    #     while True:
+    #         inRgb = qRgb.get()  # blocking call, will wait until a new data has arrived
+
+    #         # Retrieve 'bgr' (opencv format) frame
+    #         cv2.imshow("rgb", inRgb.getCvFrame())
+
+    #         if cv2.waitKey(1) == ord('q'):
+    #             break
 
 
 if __name__ == '__main__':
